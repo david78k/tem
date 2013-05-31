940dcc43016e3cdf69980c23710251a6e56dd75e
david78k <root@f31r.(none)>
2013-05-31 15:32:53 -0400

    api.sh

--- api.sh
+++ api.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 token=2b1ddd8af4944345b18eb4d96c3cfccf
 tenantId=aaba123ae4df4e1da83681d42553907a
+#f31
+tenantId=8585fee3fc42461bbd8f2e2d4c2f70e5
+#f32
+tenantId=4ba1075b88734c769006bc6840683fbd 
+
 host=10.5.147.228
+host=fg2
 #localhost
 username=admin
 password=password
@@ -14,8 +20,7 @@ password=password
     #-H "Content-type: application/json" | jq .
 
 # get token
-#curl -d '{"auth":{"passwordCredentials":{"username": "admin", "password": "4077kts."}}}' -H "Content-type: application/json" http://localhost:5000/v2.0/tokens
-#curl -d '{"auth":{"passwordCredentials":{"username": "admin", "password": "4077kts."}}}' -H "Content-type: application/json" http://localhost:8774/v2.0/tokens
+curl -d '{"auth":{"passwordCredentials":{"username": "admin", "password": "password"}}}' -H "Content-type: application/json" http://$host:5000/v2.0/tokens
 
 # get tenant id
 #curl -H "X-Auth-Token:$token" http://localhost:5000/v2.0/tenants
@@ -25,12 +30,12 @@ password=password
 #curl -k -X 'POST' -v http://$host:5000/v2.0/tokens -d '{"auth":{"passwordCredentials":{"username": "$username", "password":"$password"}, "tenantId":"$tenantId"}}' -H 'Content-type: application/json'
 
 # get instance list
-xauthtoken="X-Auth-Token:$token"
-echo $xauthtoken
+#xauthtoken="X-Auth-Token:$token"
+#echo $xauthtoken
 #curl -v -H "X-Auth-Token:aaba123ae4df4e1da83681d42553907a" http://localhost:8774/v2/$tenantId/servers
 #curl -v -H "$xauthtoken" http://localhost:8774/v2/$tenantId/servers
 #curl -v -H "X-Auth-Token:\$token" http://localhost:8774/v2/$tenantId/servers
-curl -v -H "X-Auth-Token:aaba123ae4df4e1da83681d42553907a" http://localhost:8774/v2/4ed43001453a478eadbe101353fd175c/servers
+#curl -v -H "X-Auth-Token:aaba123ae4df4e1da83681d42553907a" http://$host:8774/v2/4ed43001453a478eadbe101353fd175c/servers
 #curl -v -H "X-Auth-Token:2b1ddd8af4944345b18eb4d96c3cfccf" http://10.5.147.228:8774/v2/4ed43001453a478eadbe101353fd175c/servers
 
 # get image-list
